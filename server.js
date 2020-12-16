862903407afceaea0e00f7aba618790799fe89d2
asma <faouzi.asma@gmail.com>
2020-12-16 23:35:06 +0100

    add Procfile

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-//Install express server
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-oroeml-bzivrf'));
+app.use(express.static('./dist/angular-app-tp'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/angular-oroeml-bzivrf/index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/angular-app-tp' }
+  );
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
